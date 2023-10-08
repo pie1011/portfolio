5550b9a578bc28d65653e20c09bfac556a368ca1
Katie <pie10101011@gmail.com>
2023-10-08 14:28:26 -0700

    changed submission 2

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Contact.css";
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';

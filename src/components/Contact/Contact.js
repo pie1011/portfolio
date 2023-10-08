d493fd71e6d8a3f8c1fc64bf8cefe2511914051e
Katie <pie10101011@gmail.com>
2023-10-08 13:26:37 -0700

    added contact and reCAPTCHA

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Contact.css";
-import Email from "./Email";
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 
 const Contact = (props) => {
@@ -13,12 +15,31 @@ const Contact = (props) => {
             <Container className="portfolio-contact-container d-flex flex-column flex-lg-row">
 
                 <div className="p-1 p-lg-5 text-center">
-                <h2 className="display-1 name-header mb-3">Contact</h2>
+                    <h2 className="display-1 name-header mb-3">Contact</h2>
                 </div>
 
-                <div className="p-1 p-lg-5">
-                    <Email />
-                </div>
+                <Container fluid className="p-1 p-lg-5">
+                    <Form name="contact" method="POST" netlify>
+                        <Form.Group className="mb-3" controlId="contactForm.Name" data-bs-theme="dark">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="name@example.com" className="portfolio-input" />
+                        </Form.Group>
+                        <Form.Group className="mb-5" controlId="contactForm.Email" data-bs-theme="dark">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" className="portfolio-input" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="contactForm.Subject" data-bs-theme="dark">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control type="text" placeholder="name@example.com" className="portfolio-input" />
+                        </Form.Group>
+                        <Form.Group className="mb-5" controlId="contactForm.Message" data-bs-theme="dark">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control as="textarea" rows={3} className="portfolio-input" />
+                            <div className="g-recaptcha" data-sitekey="6LeGLOEdAAAAACIfTEFMm4j_jMg3IVmdd-KLzLQ0"></div>
+                            <Button className="portfolio-button mt-5" type="submit"><i class="bi bi-send-fill"></i> Send</Button>
+                        </Form.Group>
+                    </Form>
+                </Container>
 
             </Container>
             <Container fluid className="portfolio-down mt-auto pb-5 mx-auto text-white-50 text-center">

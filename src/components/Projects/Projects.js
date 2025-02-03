219416eae3cfe9fe4905ad9285e7070fa8da5407
Katie <katieharshman1011@gmail.com>
2025-02-03 05:13:50 +0000

    Added arrow to portfolio section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,11 @@ const Projects = (props) => {
                 <h3 className="text-center">Coming soon!</h3>
                 <p className="text-center">In the meantime, check out this <a href="https://github.com/pie1011/salon" target="blank">mock salon</a> I made, or my <a href="https://github.com/pie1011" target="blank">github</a>!</p>
             </Container>
+            <Container fluid className="portfolio-down mt-auto pb-5 mx-auto text-white-50 text-center">
+                <a href="#Contact" className="down-arrow" aria-label="Move to the Contact section.">
+                    <i className="bi bi-arrow-down-circle-fill"></i>
+                </a>
+            </Container>
         </Container>
     )
 }

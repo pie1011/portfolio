58d71a3f6ae3e2ad0093a66afb6a45d0fd246c42
Katie <katieharshman1011@gmail.com>
2025-02-10 03:02:50 +0000

    added project management to projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,8 +9,10 @@ const Projects = (props) => {
         <Container id="Projects" className="portfolio-projects d-flex flex-column min-vw-100 min-vh-100">
             <Container className="fluid my-auto">
                 <h2 className="display-1 name-header text-center">Projects</h2>
-                <h3 className="text-center">Coming soon!</h3>
-                <p className="text-center">In the meantime, check out this <a href="https://github.com/pie1011/salon" target="blank">mock salon</a> I made, or my <a href="https://github.com/pie1011" target="blank">github</a>!</p>
+                <p className="text-center pt-5">View business documentation projects from my project management coursework in my <a href="https://sites.google.com/view/project-management-portfolio/" target="blank">Project Management Portfolio</a>!</p>
+
+                <h3 className="text-center"></h3>
+                <p className="text-center">Software development project portfolio is under construction. In the meantime, check out this <a href="https://github.com/pie1011/salon" target="blank">mock salon</a> I made, or my <a href="https://github.com/pie1011" target="blank">github</a>!</p>
             </Container>
             <Container fluid className="portfolio-down mt-auto pb-5 mx-auto text-white-50 text-center">
                 <a href="#Contact" className="down-arrow" aria-label="Move to the Contact section.">

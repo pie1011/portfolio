e7ca33b9f82a250d84f7b2536357100bc109ef98
Katie <katieharshman1011@gmail.com>
2025-02-10 03:21:25 +0000

    fixed headers

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,6 @@ const Projects = (props) => {
                 <h2 className="display-1 name-header text-center">Projects</h2>
                 <p className="text-center pt-5">View business documentation projects from my project management coursework in my <a href="https://sites.google.com/view/project-management-portfolio/" target="blank">Project Management Portfolio</a>!</p>
 
-                <h3 className="text-center"></h3>
                 <p className="text-center">Software development project portfolio is under construction. In the meantime, check out this <a href="https://github.com/pie1011/salon" target="blank">mock salon</a> I made, or my <a href="https://github.com/pie1011" target="blank">github</a>!</p>
             </Container>
             <Container fluid className="portfolio-down mt-auto pb-5 mx-auto text-white-50 text-center">
